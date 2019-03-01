ba57df4184bd1d34d9af8e0bac3c35fc46d8ef30
SilentDirewolf <carlbai66@gmial.com>
2019-02-28 19:45:42 -0800

    fixing bugs and add evaluation results

--- docIndex.py
+++ docIndex.py
@@ -36,7 +36,7 @@ def getDocInfo(file):
 # file = "data2/query_list.txt"
 def getQuery(file):
     nums = []
-    queries = {}
+    queries = []
 
     f = open(file, 'r+')
 
@@ -45,22 +45,86 @@ def getQuery(file):
         a = line.split()
 
         #obtain the query-number in the file
-        num = a[0]
-        nums.append(int(num))
+        num = int(a[0])
+        nums.append(num)
 
         #obtain the query-text in the file
         query = line.strip(a[0]).strip()
-        queries[int(num)] = query
+        queries.append([num, query])
 
-    queries = collections.OrderedDict(sorted(queries.items(), key=lambda t: t[0]))
     return queries
 
 
+'''
 def queryFreq(query, term):
     freq = 0
     for term in query[1].split():
         freq += 1
     return freq
+'''
+
+
+def queryRetrieval(file):
+    stop_words = set(open("stoplist.txt").read().split("\n"))
+
+    # file = "data2/query_list.txt"
+    files = getQuery(file)
+    query_ids = []
+    for file in files:
+        query_id = file[0]
+        query_ids.append(query_id)
+
+    queryIndex = {docID: 0 for docID in query_ids}
+    # query_id: the number of terms in that document
+
+    postings_words = {}
+
+    terms = set([])
+    # search all the terms
+    for file in files:
+        query_id = file[0]
+        new_text = file[1].translate(translator)
+        temp_words = word_tokenize(new_text)
+        for n, i in enumerate(temp_words):
+            temp_words[n] = i.lower()
+            temp_words[n] = ps.stem(i)
+        temp_terms = set(temp_words)
+        temp_terms -= stop_words
+        queryIndex[query_id] = len(temp_terms)
+        terms = terms | temp_terms
+        postings_words[query_id] = temp_terms
+
+    queryIndex = collections.OrderedDict(sorted(queryIndex.items(), key=lambda t: t[0]))
+    postings = {term: [[0, 0] for i in range(len(query_ids))] for term in terms}
+    # form of posting: {term1:[[docID1,docFreq1],[docID2,docFreq2],...],}
+    numQuery = {term: 0 for term in terms}
+    # form of numDocs: {term: the number of documents that contain the term}
+
+    queryNum = 0
+
+    for file in files:
+        query_id = file[0]
+        words = word_tokenize(file[1].translate(translator))
+        for n, i in enumerate(words):
+            words[n] = i.lower()
+            words[n] = ps.stem(i)
+        for word in words:
+            if word not in stop_words:
+                postings[word][queryNum][0] = query_id
+                postings[word][queryNum][1] += 1
+        queryNum += 1
+
+    for term in terms:
+        new_postings = []
+        for posting in postings[term]:
+            if not posting[1] == 0:
+                new_postings.append(posting)
+        postings[term] = new_postings
+        numQuery[term] = len(new_postings)
+
+    postings = collections.OrderedDict(sorted(postings.items(), key=lambda t: t[0]))
+
+    return terms, postings, numQuery, queryIndex, queryNum, postings_words
 
 
 def docRetrieval(file_path):
@@ -76,6 +140,8 @@ def docRetrieval(file_path):
     docIndex = {docID: 0 for docID in doc_ids}
     # doc_id: the number of terms in that document
 
+    postings_words = {}
+
     terms = set([])
     # search all the terms
     for file in files:
@@ -89,6 +155,7 @@ def docRetrieval(file_path):
         temp_terms -= stop_words
         docIndex[doc_id] = len(temp_terms)
         terms = terms | temp_terms
+        postings_words[doc_id] = temp_terms
 
     postings = {term: [[0, 0] for i in range(len(doc_ids))] for term in terms}
     # form of posting: {term1:[[docID1,docFreq1],[docID2,docFreq2],...],}
@@ -119,7 +186,7 @@ def docRetrieval(file_path):
 
     postings = collections.OrderedDict(sorted(postings.items(), key=lambda t: t[0]))
 
-    return terms, postings, numDocs, docIndex
+    return terms, postings, numDocs, docIndex, docNum, postings_words
 
 
 def docIndex(path):
--- test.py
+++ test.py
@@ -3,100 +3,123 @@ from docIndex import *
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 import string
+
 ps = PorterStemmer()
 
 
-def tf_idf_q(postings, doc_index, query, term):
+def tf_idf_q(postings_doc, doc_index, postings_query, query_index):
     tf = []
     idf = []
-    tf_idf = []
-    count_terms = 0  # count the number of terms in the doc
+    tf_idf_temp = []
+    tf_idf_query = []
+    ids = []
+    count_terms = 0  # count the number of terms in the query
     count_doc = 0  # count the number of doc in which term occurs
 
-    for posting in postings:
+    for posting_doc in postings_doc:
         count_doc += 1
-    print(count_doc)
-    for posting in postings:
-        count_terms = doc_index[posting[0]]
+    # print(count_doc)
+
+    for posting_query in postings_query:
+        count_terms = query_index[posting_query[0]]
         # print(count_terms)
-        tf.append(round(queryFreq(query, term), 8) / round(count_terms, 8))
-        # print(round(posting[1], 8) / round(count_terms, 8))
+        tf.append(round(posting_query[1], 8) / round(count_terms, 8))
+        ids.append(posting_query[0])
+        # print(round(posting_doc[1], 8) / round(count_terms, 8))
         idf.append(log2(len(doc_index) / count_doc))
 
-    tf_idf = [a * b for a, b in zip(tf, idf)]
-    return tf_idf
+    tf_idf_temp = [a * b for a, b in zip(tf, idf)]
+    for i in range(len(ids)):
+        tf_idf_query.append([ids[i], tf_idf_temp[i]])
+    return tf_idf_query
 
 
-def tf_idf(postings, doc_index):
-    tf = []
-    idf = []
-    tf_idf = []
+def tf_idf_d(postings, doc_index):
+    tf_idf_doc = []
     count_terms = 0  # count the number of terms in the doc
     count_doc = 0  # count the number of doc in which term occurs
 
     for posting in postings:
         count_doc += 1
-    print(count_doc)
+    # print(count_doc)
     for posting in postings:
         count_terms = doc_index[posting[0]]
         # print(count_terms)
-        tf.append(round(posting[1], 8) / round(count_terms, 8))
+        tf = round(posting[1], 8) / round(count_terms, 8)
         # print(round(posting[1], 8) / round(count_terms, 8))
-        idf.append(log(len(doc_index) / count_doc))
+        idf = log(len(doc_index) / count_doc)
+        tf_idf_doc.append([posting[0], tf * idf])
 
-    tf_idf = [a * b for a, b in zip(tf, idf)]
-    return tf_idf
-
-
-def CosineScore(query, postings,terms):
-    score = 0
-    length = 0
-
-    file =  input("Please enter an abosolute file path to ")
-    weights_p = {}
-    for query in getQuery(file):
-        temp_words = word_tokenize(query)
-        for n, i in enumerate(temp_words):
-            temp_words[n] = i.lower()
-            temp_words[n] = ps.stem(i)
-
-        for term in terms:
-
-    '''
-    for i in range(len(postings)):
-        print(f"result in document file{postings[i][0]:d}: tf:{tf[i]:f}; idf:{idf[i]:f}; tf-idf:{tf_idf[i]:f}")
-    '''
+    return tf_idf_doc
 
 
-# path = "/Users/27422/PycharmProjects/HW/data"
-'''
-path = input("Enter an absolute path to data")
-(terms, postings, numDocs, docIndex) = docIndex(path)
-'''
+def CosineScore():
+    #file1 = "data2/ap89_collection"
+    file_d = input("Please enter an absolute file path to doc you wanna search ")
+    (terms_d, postings_d, numDocs_d, docIndex_d, docNum_d, postings_wd) = docRetrieval(file_d)
+
+    #file2 = "data2/query_list.txt"
+    file_q = input("Please enter an absolute file path to the query ")
+    (terms_q, postings_q, numDocs_q, queryIndex_q, queryNum_q, postings_wq) = queryRetrieval(file_q)
+
+    score = {key: 0 for key in docIndex_d}
+    length_d = {}
+    length_q = {}
+    weights_d = {key: [] for key in docIndex_d}
+    weights_q = {key: [] for key in queryIndex_q}
+
+    for term_d in terms_d:
+        array = tf_idf_d(postings_d[term_d], docIndex_d)
+        for i in range(len(array)):
+            weights_d[array[i][0]].append([term_d, array[i][1]])
+
+    for term_q in terms_q:
+        if term_q in terms_d:
+            array = tf_idf_q(postings_d[term_q], docIndex_d, postings_q[term_q], queryIndex_q)
+            for i in range(len(array)):
+                weights_q[array[i][0]].append([term_q, array[i][1]])
+        else:
+            for array in postings_q[term_q]:
+                weights_q[array[0]].append([term_q, 0])
+
+    for id_d in weights_d:
+        s = 0
+        for i in range(len(weights_d[id_d])):
+            s += pow(weights_d[id_d][i][1], 2)
+        length_d[id_d] = sqrt(s)
+
+    for id_q in weights_q:
+        s = 0
+        for i in range(len(weights_q[id_q])):
+            s += pow(weights_q[id_q][i][1], 2)
+        length_q[id_q] = sqrt(s)
+
+    fp = open('data2/results_file.txt', 'w+')
+
+    for index_q in queryIndex_q:
+        for index_d in docIndex_d:
+            for term_q in postings_wq[index_q]:
+                if term_q in postings_wd[index_d]:
+                    for i in range(len(weights_q[index_q])):
+                        if weights_q[index_q][i][0] == term_q:
+                            weight_q = weights_q[index_q][i][1]
+                    for j in range(len(weights_d[index_d])):
+                        if weights_d[index_d][j][0] == term_q:
+                            weight_d = weights_d[index_d][j][1]
+                    score[index_d] = score[index_d] + weight_d * weight_q
+            score[index_d] = score[index_d] / (length_d[index_d] * length_q[index_q])
+        score = collections.OrderedDict(sorted(score.items(), reverse=True, key=lambda t: t[1]))
+        i = 0
+        for DocNO in score:
+            i += 1
+            if score[DocNO] != 0:
+                fp.write('{query_number} Q0 {DocNO} {rank} {score} Exp\n'.format(query_number=index_q, DocNO=DocNO, rank=i,
+                                                                        score=score[DocNO]))
+                print('{query_number} Q0 {DocNO} {rank} {score} Exp'.format(query_number=index_q, DocNO=DocNO, rank=i,
+                                                                        score=score[DocNO]))
+        score = {key: 0 for key in docIndex_d}
+
+
 
 if __name__ == "__main__":
-    '''
-    while (1):
-        term = input("Enter a term: (Enter QUIT for termination) ")
-        if term == 'QUIT':
-            break
-        if term in terms:
-            print(postings[term])
-            tf_idf(postings[term], numDocs[term], docIndex)
-        if term not in terms:
-            print("\'%s\' does not appear in the collections. Please re-enter a term" % term)
-    
-    for i in getDocInfo(file="/Users/27422/PycharmProjects/HW/data2/ap89_collection"):
-        print(i)
-
-    new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
-    words = word_tokenize(new_text)
-    print(words)
-
-    for w in words:
-        print(ps.stem(w))
-   print(getQuery('data2/query_list.txt'))
-'''
-    path = "data2/ap89_collection"
-    print(docRetrieval(path))
-
+    CosineScore()
